be7d6403c5ad5467302c0c2e5511885c8d28f14b
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-09-05 18:57:53 -0300

    fix: disable user_id

--- src/app/shared/dialogs/dialog-order/dialog-order.component.ts
+++ src/app/shared/dialogs/dialog-order/dialog-order.component.ts
@@ -1,24 +1,23 @@
-import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
 import {afterNextRender, Component, inject, Inject, Injector, signal, ViewChild} from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {ApiResponse, PaymentForm} from '@models/application';
-import { Construction } from '@models/construction';
-import { Supplier, SupplierType } from '@models/supplier';
+import {Construction} from '@models/construction';
+import {Supplier, SupplierType} from '@models/supplier';
 import {Banco, OrderResponsible, RequestOrder, RequestOrderStatus, RequestOrderType} from '@models/requestOrder';
-import { User } from '@models/user';
-import { ConstructionService } from '@services/construction.service';
-import { OrderService } from '@services/order.service';
-import { SupplierService } from '@services/supplier.service';
-import { UserService } from '@services/user.service';
+import {User} from '@models/user';
+import {ConstructionService} from '@services/construction.service';
+import {OrderService} from '@services/order.service';
+import {SupplierService} from '@services/supplier.service';
+import {UserService} from '@services/user.service';
 import dayjs from 'dayjs';
-import { ToastrService } from 'ngx-toastr';
-import { finalize } from 'rxjs';
-import { dateValidator } from '@shared/validators/date';
-import { DialogOrderSolicitationComponent } from '../dialog-order-solicitation/dialog-order-solicitation.component';
-import { RequestService } from '@services/request.service';
-import { RequestStatus } from '@models/request';
-import { SessionQuery } from '@store/session.query';
+import {ToastrService} from 'ngx-toastr';
+import {finalize} from 'rxjs';
+import {DialogOrderSolicitationComponent} from '../dialog-order-solicitation/dialog-order-solicitation.component';
+import {RequestService} from '@services/request.service';
+import {RequestStatus} from '@models/request';
+import {SessionQuery} from '@store/session.query';
 
 @Component({
   selector: 'app-dialog-order',
@@ -27,25 +26,25 @@ import { SessionQuery } from '@store/session.query';
 })
 export class DialogOrderComponent {
 
-  public loading : boolean = false;
-  public title : string = 'Novo Pedido';
-  protected isNewOrder : boolean = true;
-  protected isToEdit : boolean = false;
+  public loading: boolean = false;
+  public title: string = 'Novo Pedido';
+  protected isNewOrder: boolean = true;
+  protected isToEdit: boolean = false;
 
-  protected form : FormGroup;
+  protected form: FormGroup;
 
   protected selectedFiles: File[] = [];
-  protected filesToSend : {
+  protected filesToSend: {
     id: number,
     preview: string,
     file: File,
   }[] = [];
 
-  protected filesToRemove : number[] = [];
-  protected filesFromBack : {
-    index : number,
+  protected filesToRemove: number[] = [];
+  protected filesFromBack: {
+    index: number,
     id: number,
-    name : string,
+    name: string,
     path: string, // Wasabi
   }[] = [];
 
@@ -55,30 +54,31 @@ export class DialogOrderComponent {
   protected requestOrderPaymentSelection = Object.values(PaymentForm);
   protected requestResponsibleSelection = Object.values(OrderResponsible);
 
-  protected constructions : Construction[] = [];
-  protected users : User[] = [];
-  protected suppliers : Supplier[] = [];
-  protected typeSuppliers : SupplierType[] = [];
+  protected constructions: Construction[] = [];
+  protected users: User[] = [];
+  protected suppliers: Supplier[] = [];
+  protected typeSuppliers: SupplierType[] = [];
 
   public allowedTypes = ['image/png', 'image/jpeg', 'image/jpg', 'application/pdf'];
 
   public bancos = signal<Banco[]>([]);
   public categories = signal<any[]>([]);
 
-  public isAdmin = false; 
-  public hasGranatum = false; 
+  public isAdmin = false;
+  public hasGranatum = false;
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
     protected readonly _data,
     private readonly _dialogRef: MatDialogRef<DialogOrderComponent>,
-    private readonly _fb : FormBuilder,
-    private readonly _toastr : ToastrService,
-    private readonly _orderService : OrderService,
-    private readonly _solicitationService : RequestService,
-    private readonly _constructionService : ConstructionService,
-    private readonly _userService : UserService,
-    private readonly _supplierService : SupplierService,
-    private readonly _sessionQuery : SessionQuery,
+    private readonly _fb: FormBuilder,
+    private readonly _toastr: ToastrService,
+    private readonly _orderService: OrderService,
+    private readonly _solicitationService: RequestService,
+    private readonly _constructionService: ConstructionService,
+    private readonly _userService: UserService,
+    private readonly _supplierService: SupplierService,
+    private readonly _sessionQuery: SessionQuery,
     private readonly _dialog: MatDialog,
   ) {
 
@@ -96,7 +96,7 @@ export class DialogOrderComponent {
       order_type: [null, Validators.required],
       date: [null, Validators.required],
       construction_id: [null, Validators.required],
-      user_id: [null, Validators.required],
+      user_id: [null],
       supplier_id: [null, Validators.required],
       description: [''],
       total_value: [null, Validators.required],
@@ -126,7 +126,7 @@ export class DialogOrderComponent {
         });
       }
 
-      if(!this._data.edit) {
+      if (!this._data.edit) {
         this.isToEdit = true;
         this.form.disable();
 
@@ -137,7 +137,7 @@ export class DialogOrderComponent {
 
       this._data.order.files.forEach((file, index) => {
         this.filesFromBack.push({
-          index : index,
+          index: index,
           id: file.id,
           name: file.name,
           path: file.path
@@ -146,12 +146,12 @@ export class DialogOrderComponent {
 
       this.form.patchValue(this._data.order);
     } else {
-        this.items.push(this.createItem());
+      this.items.push(this.createItem());
     }
 
     // Autoupdate total_value e quantity_items
     this.form.get('items').valueChanges.subscribe((items) => {
-      if(!items[items.length - 1]?.unit_value && !items[items.length - 1]?.quantity) return
+      if (!items[items.length - 1]?.unit_value && !items[items.length - 1]?.quantity) return
 
       this.form.get('quantity_items').setValue(0);
       this.form.get('total_value').setValue(0);
@@ -170,26 +170,27 @@ export class DialogOrderComponent {
 
   }
 
-  public loadPermissions(){
+  public loadPermissions() {
     this._sessionQuery.user$.subscribe(user => {
-      if(user && user?.company_position.position !== 'Requester') {
+      if (user && user?.company_position.position !== 'Requester') {
         this.isAdmin = true;
-      }else{
-        this.form.get('purchase_status').disable();        
+      } else {
+        this.form.get('purchase_status').disable();
+        this.form.get("user_id").disable();
       }
     })
   }
 
-  public loadPermissionGranatum(){
+  public loadPermissionGranatum() {
     this._sessionQuery.user$.subscribe(user => {
-      if(user && (user?.company_position.position === 'Financial' || user?.company_position.position === 'Admin')) {
+      if (user && (user?.company_position.position === 'Financial' || user?.company_position.position === 'Admin')) {
         this.hasGranatum = true;
       }
     })
   }
 
-  public postOrder(order : RequestOrder) {
-    if (!this.prepareFormData(order)){
+  public postOrder(order: RequestOrder) {
+    if (!this.prepareFormData(order)) {
       this.loading = false;
       return;
     }
@@ -203,14 +204,14 @@ export class DialogOrderComponent {
           this._toastr.success('Pedido enviado com sucesso!');
           this._dialogRef.close(true);
         },
-        error : (err) => {
+        error: (err) => {
           this._toastr.error(err.error.error);
         }
       });
   }
 
-  public patchOrder(id : number, order : RequestOrder) {
-    if (!this.prepareFormData(order)){
+  public patchOrder(id: number, order: RequestOrder) {
+    if (!this.prepareFormData(order)) {
       this.loading = false;
       return;
     }
@@ -224,19 +225,19 @@ export class DialogOrderComponent {
           this._toastr.success('Pedido atualizado com sucesso!');
           this._dialogRef.close(true);
         },
-        error : (err) => {
+        error: (err) => {
           this._toastr.error(err.error.error);
         }
       });
   }
 
-  public prepareFormData(order : RequestOrder) {
-    if(!order.items.length ){
+  public prepareFormData(order: RequestOrder) {
+    if (!order.items.length) {
       this._toastr.error('Item é um campo obrigatório');
       return;
     }
 
-    if(!order.order_files.length && !this.filesFromBack.length){
+    if (!order.order_files.length && !this.filesFromBack.length) {
       this._toastr.error('Anexo é um campo obrigatório');
       return;
     }
@@ -245,27 +246,24 @@ export class DialogOrderComponent {
 
     Object.keys(order).forEach((key) => {
 
-      if(key == 'order_files') {
+      if (key == 'order_files') {
         (order.order_files).forEach(file => {
           orderFormData.append('order_files[]', file);
         });
-      }
-      else if(key == 'date') {
+      } else if (key == 'date') {
         orderFormData.append('date', dayjs(order.date).format('YYYY-MM-DD'));
-      }
-      else if(key == 'items') {
+      } else if (key == 'items') {
         (order.items).forEach(item => {
           orderFormData.append('items[]', JSON.stringify(item));
         });
-      }
-      else
+      } else
         orderFormData.append(key, order[key]);
     });
 
     return orderFormData;
   }
 
-  public onSolicitation(){
+  public onSolicitation() {
     const order: RequestOrder = this._data.order
     const data = {
       order_id: order.id,
@@ -291,50 +289,49 @@ export class DialogOrderComponent {
       });
   }
 
-  private createSolicitation(data){
+  private createSolicitation(data) {
     this._solicitationService.postRequest(data)
-    .subscribe({
-      next: () => {
-        this._toastr.success('Pedido solicitado com sucesso!');
-        this._dialogRef.close();
-      },
-      error: (err) => {
-        this._toastr.error(err.error.error);
-      },
-    });
+      .subscribe({
+        next: () => {
+          this._toastr.success('Pedido solicitado com sucesso!');
+          this._dialogRef.close();
+        },
+        error: (err) => {
+          this._toastr.error(err.error.error);
+        },
+      });
   }
 
   public onConfirm(): void {
-    if(!this.form.valid || this.loading) return;
+    if (!this.form.valid || this.loading) return;
 
     this._initOrStopLoading();
 
-    let order_files : File[] = [];
+    let order_files: File[] = [];
 
-    for(let file of this.filesToSend) {
+    for (let file of this.filesToSend) {
       order_files.push(file.file);
     }
 
-    if(this.isNewOrder) {
+    if (this.isNewOrder) {
       this.postOrder(
         {
           order_files,
           ...this.form.getRawValue()
         }
       );
-    }
-    else {
+    } else {
       this.filesToRemove.forEach(file => {
         this._orderService.deleteFile(file)
           .subscribe({
-            next : (res) => {
+            next: (res) => {
 
             },
-            error : (err) => {
+            error: (err) => {
               this._toastr.error(err.error.error);
             }
           })
-        })
+      })
 
       this.patchOrder(
         this._data.order.id,
@@ -354,7 +351,7 @@ export class DialogOrderComponent {
   // Items
   public createItem(): FormGroup {
     return this._fb.group({
-      id : [null],
+      id: [null],
       name: [null, Validators.required],
       unit_value: [null, Validators.required],
       quantity: [null, Validators.required]
@@ -364,7 +361,7 @@ export class DialogOrderComponent {
   private createItemFromData(item: any): FormGroup {
     return this._fb.group({
       id: [item.id],
-      name: [{ value: item.key}, [Validators.required]],
+      name: [{value: item.key}, [Validators.required]],
       unit_value: [item.unit_value, [Validators.required]],
       quantity: [item.type, [Validators.required]],
     });
@@ -391,17 +388,17 @@ export class DialogOrderComponent {
     }
   }
 
-  private deleteItem(index){
+  private deleteItem(index) {
     this._orderService.deleteItem(this.items.value[index].id)
-    .subscribe({
-      next: () => {
-        this._toastr.success("Item deletado com sucesso");
-        this.items.removeAt(index);
-      },
-      error: (err) => {
-        this._toastr.error(err.error.error);
-      }
-    })
+      .subscribe({
+        next: () => {
+          this._toastr.success("Item deletado com sucesso");
+          this.items.removeAt(index);
+        },
+        error: (err) => {
+          this._toastr.error(err.error.error);
+        }
+      })
   }
 
   // Files
@@ -421,7 +418,7 @@ export class DialogOrderComponent {
 
     for (const file of fileArray) {
       if (this.allowedTypes.includes(file.type)) {
-        let base64 : string = null;
+        let base64: string = null;
 
         if (file.type.startsWith('image/')) {
           base64 = await this.convertFileToBase64(file);
@@ -432,8 +429,7 @@ export class DialogOrderComponent {
           preview: base64,
           file: file,
         });
-      }
-      else
+      } else
         this._toastr.error(`${file.type} não é permitido`);
     }
   }
@@ -465,7 +461,7 @@ export class DialogOrderComponent {
         next: (res) => {
           this._toastr.success(res.message);
         },
-        error : (err) => {
+        error: (err) => {
           this._toastr.error(err.error.error);
         }
       })
