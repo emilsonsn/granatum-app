798ad10f4db83179e31381f9040f37c0b343b9cd
emilsonsn <emilsonsn2@gmail.com>
2024-09-17 09:33:40 -0300

    fix: corrige git ignore

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-git pull https://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@github.com/emilsonsn/granatum-app.git
+git pull https://github.com/emilsonsn/granatum-app.git
 
 npm install
 
