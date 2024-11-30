8d80eca266b8a541dc34d66896944b331c569886
emilsonsn <emilsonsn2@gmail.com>
2024-11-30 10:08:30 -0300

    fix: ajusta enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
   appName: 'Granatum App',
   home: '/painel/home',
-   api: 'http://127.0.0.1:8000/api',
-  //api: "https://app.andradeengenhariaeletrica.com.br:3001/api",
+  api: "https://app.andradeengenhariaeletrica.com.br:3001/api",
+  //  api: 'http://127.0.0.1:8000/api',
   version: require('../../package.json').version
 };
