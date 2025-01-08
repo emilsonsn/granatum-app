a3ec410e2af2da7aa32e2a804cb28b24302aaf7b
emilsonsn <emilsonsn2@gmail.com>
2025-01-08 09:09:12 -0300

    feat: conclui cadastro de leads

--- src/app/views/public/leads/leads.component.ts
+++ src/app/views/public/leads/leads.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { LeadService } from '@services/crm/lead.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-leads',
@@ -12,35 +14,48 @@ export class LeadsComponent {
     protected loading: boolean = false;
     protected id:number = 0;
     protected responsible_id:number = 0;
+    public isRegistered = false;
 
-      constructor(
-        private fb: FormBuilder,
-        private route: ActivatedRoute
-      ) {
-        this.form = this.fb.group({
-          id: [null],
-          name: ['', [Validators.required, Validators.maxLength(256)]],
-          email: ['', [Validators.required, Validators.email, Validators.maxLength(256)]],
-          phone: ['', [Validators.required, Validators.maxLength(256)]],
-          origin: [''],
-          observations: [''],
-          responsible_id: [null],
-        });
-      }
+    constructor(
+      private fb: FormBuilder,
+      private route: ActivatedRoute,
+      private readonly _leadService: LeadService,
+      private readonly _toastr: ToastrService,
+    ) {}
 
-      ngOnInit() {
-        this.id = +this.route.snapshot.queryParamMap.get('id');
-        this.responsible_id = +this.route.snapshot.queryParamMap.get('responsible');
-      }
+    ngOnInit() {
 
-      onSubmit(form: FormGroup) {
-        if (!form.valid) {
-          form.markAllAsTouched();
-        } else {
-          form.patchValue({
-            responsible_id: this.responsible_id,
-          });
-          console.log(this.form.getRawValue())
-        }
+      this.id = +this.route.snapshot.queryParamMap.get('id');
+      this.responsible_id = +this.route.snapshot.queryParamMap.get('responsible');
+
+      this.form = this.fb.group({
+        id: [null],
+        name: ['', [Validators.required, Validators.maxLength(256)]],
+        email: ['', [Validators.required, Validators.email, Validators.maxLength(256)]],
+        phone: ['', [Validators.required, Validators.maxLength(256)]],
+        origin: ['Formulário de cadastro'],
+        observations: ['Lead cadastrado por link de cadastro'],
+        responsible_id: [null],
+        funnel_id: [this.id]
+      });
+    }
+    onSubmit(form: FormGroup) {
+      if (!form.valid) {
+        form.markAllAsTouched();
+      } else {          
+        this._leadService.create({
+          ...form.getRawValue(),
+          responsible_id: this.responsible_id,
+        })
+        .subscribe({
+          next: (value) => {
+            this._toastr.success('Ação realizada com sucesso.');
+            this.isRegistered = true;
+          },
+          error: (error) => {
+            console.error('Error:', error);
+          }
+        })
       }
+    }
 }
--- src/app/views/public/public.module.ts
+++ src/app/views/public/public.module.ts
@@ -14,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LeadsComponent } from './leads/leads.component';
+import { MatSpinner } from '@angular/material/progress-spinner';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { LeadsComponent } from './leads/leads.component';
     SharedModule,
     MatButtonModule,
     MatDivider,
-    MatOption
+    MatOption,
+    MatSpinner
   ]
 })
 export class PublicModule { }
