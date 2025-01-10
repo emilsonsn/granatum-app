a4de85e1bd15318b1fdade55d662774a4c5b93f6
emilsonsn <emilsonsn2@gmail.com>
2025-01-10 08:45:27 -0300

    fix: configura enviroments do sistema

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,12 +3,12 @@ declare const require: any;
 export const environment = {
   production: false,
   appName: 'Granatum App',
-  home: '/painel',
+  home: '/painel/home',
   // instanceCRM: 'teste123',
   // instanceRH: 'teste321',
   instanceCRM: 'escritorio',
   instanceRH: 'mel',
-  wsUrl: 'ws://127.0.0.1:6001',
+  wsUrl: 'ws://127.0.0.1:6001/app/local',
   api: "https://app.andradeengenhariaeletrica.com.br:3001/api",
   // api: 'http://127.0.0.1:8000/api',
   version: require('../../package.json').version
